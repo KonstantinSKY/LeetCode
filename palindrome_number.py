50c09b01f9392d81a37e965a5d554d469b9db18c
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-06 22:25:54 -0800

    test added

---
+++ palindrome_number.py
@@ -0,0 +1,5 @@
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return True
+
--- tests/test_palindrome_number.py
+++ tests/test_palindrome_number.py
@@ -1,37 +1,40 @@
+import os
+import sys
 import unittest
+sys.path.append(os.pardir)
 from palindrome_number import Solution
 
 
-class DatabasesTestCase(unittest.TestCase):
+class PalindromeNumberTestCase(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
     def test_example1(self):
-        self.assertEqual(self.solution(121), True)
+        self.assertEqual(self.solution.isPalindrome(121), True)
 
     def test_example2(self):
-        self.assertEqual(self.solution(-121), False)
+        self.assertEqual(self.solution.isPalindrome(-121), False)
 
     def test_example3(self):
-        self.assertEqual(self.solution(10), False)
+        self.assertEqual(self.solution.isPalindrome(10), False)
 
     def test_example4(self):
-        self.assertEqual(self.solution(-101), False)
+        self.assertEqual(self.solution.isPalindrome(-101), False)
 
     def test_example5(self):
-        self.assertEqual(self.solution(1001), True)
+        self.assertEqual(self.solution.isPalindrome(1001), True)
 
     def test_example6(self):
-        self.assertEqual(self.solution(34433443), True)
+        self.assertEqual(self.solution.isPalindrome(34433443), True)
 
     def test_example7(self):
-        self.assertEqual(self.solution(3434435), True)
+        self.assertEqual(self.solution.isPalindrome(3434435), True)
 
     def test_example8(self):
-        self.assertEqual(self.solution(101001010), True)
+        self.assertEqual(self.solution.isPalindrome(101001010), True)
 
     def test_example9(self):
-        self.assertEqual(self.solution(54645433), False)
+        self.assertEqual(self.solution.isPalindrome(54645433), False)
 
 
 if __name__ == '__main__':
