ab66b125dafe590fe9ffdccbf6b8ed22396e2131
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-08 23:04:40 -0800

    solved

---
+++ valid_parentheses.py
@@ -0,0 +1,30 @@
+import time
+from typing import List
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        last_open = []
+        coll = {"]": "[", ")": "(", "}": "{"}
+        for char in s:
+            if char in coll.values():
+                last_open.append(char)
+                continue
+            if not last_open or last_open[-1] != coll[char]:
+                return False
+            last_open.pop(-1)
+        if last_open:
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().isValid("(){}"))
+    print(Solution().isValid("()[]{}"))
+    print(Solution().isValid("(]"))
+    print(Solution().isValid("([)]"))
+    print(Solution().isValid("{[]}"))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
