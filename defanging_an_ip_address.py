faeef22a3a8bac91f83128f8135d520517020bd3
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-10 09:44:07 -0800

    solved 2 variant

--- defanging_an_ip_address.py
+++ defanging_an_ip_address.py
@@ -7,11 +7,14 @@ class Solution:
     def defangIPaddr(self, address: str) -> str:
         return "[.]".join(address.split("."))
 
+    def defangIPaddr2(self, address: str) -> str:
+        return address.replace(".", "[.]")
 
 if __name__ == "__main__":
     start_time = time.time()
     print(Solution().defangIPaddr("255.100.50.0"))
     print(Solution().defangIPaddr("1.1.1.1"))
-
+    print(Solution().defangIPaddr2("255.100.50.0"))
+    print(Solution().defangIPaddr2("1.1.1.1"))
 
     print("--- %s seconds ---" % (time.time() - start_time))
