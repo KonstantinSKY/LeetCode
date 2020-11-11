989f9119b546f71798ba1273504460036b4e2191
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-10 22:16:04 -0800

    solved 2 variants

--- template.py
+++ template.py
@@ -4,10 +4,19 @@ import time
 from typing import List
 
 class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        new = [None] * len(s)
+        for i, index in enumerate(indices):
+            new[index] = s[i]
+        return "".join(new)
+
+    def restoreString2(self, s: str, indices: List[int]) -> str:
+        temp = {indices[i]: s[i] for i in range(len(s))}
+        return "".join([temp[i] for i in range(len(temp))])
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(Solution().
+    print(Solution().restoreString("codeleet", [4,5,6,7,0,2,1,3]))
 
     print("--- %s seconds ---" % (time.time() - start_time))
