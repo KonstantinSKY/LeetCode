6797d7c39ad8ed2d992416540dd5ca10a6585a05
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-10 22:34:14 -0800

    midfy

--- 1528._shuffle_string.py
+++ 1528._shuffle_string.py
@@ -5,13 +5,19 @@ from typing import List
 
 
 class Solution:
-    def uniqueOccurrences(self, arr: List[int]) -> bool:
-        return len(set(arr.count(num) for num in set(arr))) == len(set(arr))
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        new = [None] * len(s)
+        for i, index in enumerate(indices):
+            new[index] = s[i]
+        return "".join(new)
+
+    def restoreString2(self, s: str, indices: List[int]) -> str:
+        temp = {indices[i]: s[i] for i in range(len(s))}
+        return "".join([temp[i] for i in range(len(temp))])
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(Solution().uniqueOccurrences([1, 2, 2, 1, 1, 3]))
-    print(Solution().uniqueOccurrences([1, 2]))
-    print(Solution().uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]))
+    print(Solution().restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]))
+
     print("--- %s seconds ---" % (time.time() - start_time))
