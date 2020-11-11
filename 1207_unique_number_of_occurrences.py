f814c45f237a37cb3477a7139f2fac4f95bd4c98
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-10 21:27:12 -0800

    1 line solution

---
+++ 1207_unique_number_of_occurrences.py
@@ -0,0 +1,17 @@
+"""1207 https://leetcode.com/problems/unique-number-of-occurrences/ """
+
+import time
+from typing import List
+
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        return len(set(arr.count(num) for num in set(arr))) == len(set(arr))
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().uniqueOccurrences([1, 2, 2, 1, 1, 3]))
+    print(Solution().uniqueOccurrences([1, 2]))
+    print(Solution().uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]))
+    print("--- %s seconds ---" % (time.time() - start_time))
