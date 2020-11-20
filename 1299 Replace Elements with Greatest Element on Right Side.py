33dd62eed96338e20482f8f88231062465a3c70f
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-19 22:24:00 -0800

    1299 faster solved

--- 1299 Replace Elements with Greatest Element on Right Side.py
+++ 1299 Replace Elements with Greatest Element on Right Side.py
@@ -3,17 +3,22 @@
 import time
 from typing import List
 
+
 class Solution:
-    def replaceElements(self, arr: List[int]) -> List[int]:
-        for i in range(len(arr)-1):
-            arr[i] = max(arr[i+1:])
+    def replaceElements2(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr) - 1):
+            arr[i] = max(arr[i + 1:])
         arr[-1] = -1
         return arr
+
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        new = [max(arr[i + 1:]) for i in range(len(arr) - 1)]
+        new.append(-1)
+        return new
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(Solution().replaceElements([17,18,5,4,6,1]))
+    print(Solution().replaceElements([17, 18, 5, 4, 6, 1]))
 
     print("--- %s seconds ---" % (time.time() - start_time))
-
