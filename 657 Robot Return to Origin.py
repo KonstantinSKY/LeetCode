8f53848881c35e4b6460a72050e88a5a4c0d86a9
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-19 23:04:44 -0800

    675

--- 657 Robot Return to Origin.py
+++ 657 Robot Return to Origin.py
@@ -18,13 +18,16 @@ class Solution:
                 place[1] += 1
         return place[0] == 0 and place[1] == 0
 
-    def judgeCircle(self, moves: str) -> bool:
+    def judgeCircle3(self, moves: str) -> bool:
         place = [0, 0]
         steps = {"L": [-1, 0], "R": [1, 0], "U": [0, +1], "D": [0, -1]}
         for step in moves:
             place[0] += steps[step][0]
             place[1] += steps[step][1]
         return place[0] == 0 and place[1] == 0
+
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count("U") == moves.count("D") and moves.count("L") == moves.count("L")
 
 
 if __name__ == "__main__":
