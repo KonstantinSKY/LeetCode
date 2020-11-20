d564e1a050a3f2ab8712f419c7fbfd96afd6ecaf
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-19 22:37:44 -0800

    961 solved

---
+++ 961 N-Repeated Element in Size 2N Array.py
@@ -0,0 +1,18 @@
+""" 961 https://leetcode.com/problems/n-repeated-element-in-size-2n-array/"""
+
+import time
+from typing import List
+
+
+class Solution:
+    def repeatedNTimes(self, A: List[int]) -> int:
+        for num in A:
+            if A.count(num) == len(A) / 2:
+                return num
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4]))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
