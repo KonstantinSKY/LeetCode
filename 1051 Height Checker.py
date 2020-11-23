e5f7f40ede8062118ced9f1363eecf5623377b71
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-22 23:13:23 -0800

    1051 2 variant solved

--- 1051 Height Checker.py
+++ 1051 Height Checker.py
@@ -5,10 +5,16 @@ from typing import List
 
 
 class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
+    def heightChecker2(self, heights: List[int]) -> int:
         new_arr = sorted(heights)
         return sum([1 for i in range(len(heights)) if heights[i] != new_arr[i]])
 
+    def heightChecker(self, heights: List[int]) -> int:
+        res = 0
+        for i, j in zip(heights, sorted(heights)):
+            if i != j:
+                res += 1
+        return res
 
 if __name__ == "__main__":
     start_time = time.time()
