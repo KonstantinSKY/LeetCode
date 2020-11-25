912816c99481c19d6d401b385daa80036a80628a
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-25 15:33:50 -0800

    1337 first variant solved

---
+++ 1337 The K Weakest Rows in a Matrix.py
@@ -0,0 +1,18 @@
+"""1337. The K Weakest Rows in a Matrix https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/ """
+
+import time
+from typing import List
+
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        n = sorted([(mat[i], i) for i in range(len(mat))])
+        return [n[i][1] for i in range(k)]
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().kWeakestRows([[1, 1, 0, 0, 0], [1, 1, 1, 1, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [1, 1, 1, 1, 1]],
+                                  3))
+    print(Solution().kWeakestRows([[1, 0, 0, 0], [1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 0]], 2))
+    print("--- %s seconds ---" % (time.time() - start_time))
