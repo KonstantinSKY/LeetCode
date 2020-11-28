0c06aff99a4488944e667bf76f93f3f47321556c
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-27 23:36:25 -0800

    1399 solved

---
+++ 1399 Count Largest Group.py
@@ -0,0 +1,32 @@
+"""1399. Count Largest Group  https://leetcode.com/problems/count-largest-group/ """
+
+import time
+from typing import List
+
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        from collections import defaultdict
+        nums = defaultdict(list)
+        maxx = res = 0
+        for i in range(1, n+1):
+            r = i
+            summ = 0
+            while r > 0:
+                summ += r % 10
+                r //= 10
+            nums[summ].append(i)
+            length = len(nums[summ])
+            if length == maxx:
+                res += 1
+            elif length > maxx:
+                maxx = length
+                res = 1
+        return res
+
+
+        print(nums)
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().countLargestGroup(24))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
