08e932836006569374625f4487660563e1e735dc
Konstantin Smolnikov <sky012877@gmail.com>
2020-11-29 22:04:13 -0800

    136 variant 2

--- 136 Single Number.py
+++ 136 Single Number.py
@@ -4,13 +4,20 @@ import time
 from typing import List
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber2(self, nums: List[int]) -> int:
         return sorted(nums, key=lambda n: nums.count(n))[0]
 
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(0, len(nums)-1, 2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+        return nums[-1]
 
 if __name__ == "__main__":
     start_time = time.time()
     print(Solution().singleNumber([1,2,4, 1,2]))
     print(Solution().singleNumber([2,2,1]))
+    print(Solution().singleNumber([4, 1, 2, 1, 2]))
 
     print("--- %s seconds ---" % (time.time() - start_time))
