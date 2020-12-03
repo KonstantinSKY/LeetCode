04846ccda32d8c1465c416faf2f7fbf4444ae1b4
Konstantin Smolnikov <sky012877@gmail.com>
2020-12-02 23:49:14 -0800

    859 1 variant solved

---
+++ 859 Buddy Strings.py
@@ -0,0 +1,40 @@
+"""859. Buddy Strings https://leetcode.com/problems/buddy-strings/ """
+
+import time
+from typing import List
+
+
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        if len(A) != len(B):
+            return False
+
+        idx = []
+        flag = False
+        for i in range(len(A)):
+            if A[i] != B[i]:
+                idx.append(i)
+            if len(idx) > 2:
+                return False
+            if len(idx) == 0:
+                if A.count(A[i]) > 1:
+                    flag = True
+
+        if len(idx) == 2 and A[idx[0]] == B[idx[1]] and A[idx[1]] == B[idx[0]]:
+            return True
+
+        if len(idx) == 0 and flag:
+            return True
+
+        return False
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    print(Solution().buddyStrings("ab", "ba"))
+    print(Solution().buddyStrings("ab", "bas"))
+    print(Solution().buddyStrings("aa", "aa"))
+    print(Solution().buddyStrings("ab", "ab"))
+    print(Solution().buddyStrings("abcaa", "abcbb"))
+    print("--- %s seconds ---" % (time.time() - start_time))
