a1d6136eb1fda5d52c48055b0d489721e6ee50e0
Konstantin Smolnikov <sky012877@gmail.com>
2020-12-03 21:27:59 -0800

    NO MESSAGE

--- 263 Ugly Number.py
+++ 263 Ugly Number.py
@@ -5,54 +5,32 @@ from typing import List
 
 class Solution:
     def isUgly(self, num: int) -> bool:
-
-
-
-        
-        Ans = []
-        d = 2
-        while d * d <= num:
-            print(num)
-            print("d", d)
-
-            if num % d == 0:
-                # Ans.append(d)
-                # print (d)
-                if d > 5:
-                    return False
+        n = num
+        if n == 1:
+            return True
+        for i in [2, 3, 5]:
+            while n and n != 1:
+                if n % i != 0:
+                    break
                 else:
-                    num //= d
-            else:
-                d += 1
-        if num > 1:
-            Ans.append(num)
-        return True
+                    n = n // i
+        return n == 1
 
 
-
-        # factors = [2, 3, 5]
-        # res = False
-        # for factor in factors:
-        #     if num % factor == 0:
-        #         if num // factor not in factors:
-        #             return False
-        #         else:
-        #             res = True
-        # return res
         #
 
 if __name__ == "__main__":
     start_time = time.time()
-    # print(Solution().isUgly(8))
+    print(Solution().isUgly(8))
     print(Solution().isUgly(14))
-    # # print(Solution().isUgly(4))
-    # print(Solution().isUgly(14))
-    # # print(Solution().isUgly(20))
-    # # print(Solution().isUgly(9))
-    # print(Solution().isUgly(13))
-    # # print(Solution().isUgly(7))
-    # print(Solution().isUgly(8))
-    # print(Solution().isUgly(-8))
-    # # print(Solution().isUgly(16))
-    # # print(Solution().isUgly(-2147483648))
+    print(Solution().isUgly(4))
+    print(Solution().isUgly(14))
+    print(Solution().isUgly(20))
+    print(Solution().isUgly(9))
+    print(Solution().isUgly(13))
+    print(Solution().isUgly(7))
+    print(Solution().isUgly(8))
+    print(Solution().isUgly(-8))
+    print(Solution().isUgly(16))
+    print(Solution().isUgly(-2147483648))
     print("--- %s seconds ---" % (time.time() - start_time))
