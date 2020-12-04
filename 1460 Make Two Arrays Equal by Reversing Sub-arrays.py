a9ec674587e6f9902052c615ab85b99079fdfd83
Konstantin Smolnikov <sky012877@gmail.com>
2020-12-04 15:38:39 -0800

    1460 1 liner solved

---
+++ 1460 Make Two Arrays Equal by Reversing Sub-arrays.py
@@ -0,0 +1,19 @@
+""" 1460. Make Two Arrays Equal by Reversing Sub-arrays https://leetcode.com/problems/make-two-arrays-equal-by-reversing-sub-arrays/"""
+
+import time
+from typing import List
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        # target.sort()
+        # arr.sort()
+        # print ("".join(map(str, arr)))
+        return "".join(map(str, sorted(arr))) == "".join(map(str, sorted(target)))
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().canBeEqual([1, 2, 3, 4], [2, 4, 1, 3]))
+    print(Solution().canBeEqual([3, 7, 9], [3, 7, 11]))
+    print("--- %s seconds ---" % (time.time() - start_time))
