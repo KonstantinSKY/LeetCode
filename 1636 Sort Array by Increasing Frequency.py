c000e814118763a467ba33c9963d5ce9d6e5be75
Konstantin Smolnikov <sky012877@gmail.com>
2020-12-08 22:01:09 -0800

    1636

---
+++ 1636 Sort Array by Increasing Frequency.py
@@ -0,0 +1,19 @@
+"""1636. Sort Array by Increasing Frequency https://leetcode.com/problems/sort-array-by-increasing-frequency/ """
+
+import time
+from typing import List
+
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        return sorted(sorted(nums, reverse=True), key=lambda x: nums.count(x))
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().frequencySort([1, 1, 2, 2, 2, 3]))
+    print(Solution().frequencySort([2, 3, 1, 3, 2]))
+    print(Solution().frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1]))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
