0c10f82e5d61fefe2571a513c94b3ef52047fb85
SKY <sky012877@gmail.com>
2023-04-03 11:07:09 -0700

    1486_XOR_Operation_in_an_Array.py - Added :

---
+++ 1486_XOR_Operation_in_an_Array.py
@@ -0,0 +1,20 @@
+"""1486 XOR Operation in an Array """
+
+import time
+from typing import List
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        r = start
+        for i in range(1, n):
+            r = r ^ (start + 2*i)
+        return r
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().xorOperation(5, 0))
+    print(Solution().xorOperation(4, 3))
+    print(Solution().xorOperation(1, 7))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
