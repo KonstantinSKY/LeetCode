1bc40773f71f10369a5013cd1e5cd83d9d98fca0
SKY <sky012877@gmail.com>
2023-04-03 11:07:13 -0700

    1672_Richest_Customer_Wealth.py - Added :

---
+++ 1672_Richest_Customer_Wealth.py
@@ -0,0 +1,21 @@
+"""1672. Richest Customer Wealth https://leetcode.com/problems/richest-customer-wealth/ """
+
+import time
+from typing import List
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max([sum(a) for a in accounts])
+
+    def maximumWealth2(self, accounts: List[List[int]]) -> int:
+        return max(map(sum, accounts))
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().maximumWealth([[1, 2, 3], [3, 2, 1]]))
+    print(Solution().maximumWealth([[1, 5], [7, 3], [3, 5]]))
+    print(Solution().maximumWealth([[2, 8, 7], [7, 1, 3], [1, 9, 5]]))
+
+    print("--- %s seconds ---" % (time.time() - start_time))
