e60da59468a1c337a3531dc864b6e8beea9afe4d
SKY <sky012877@gmail.com>
2023-04-03 11:07:13 -0700

    Minimum_Subsequence_in_Non-Increasing_Order.py - Added :

---
+++ Minimum_Subsequence_in_Non-Increasing_Order.py
@@ -0,0 +1,33 @@
+"""1403 https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/ """
+
+import time
+from typing import List
+
+
+class Solution:
+    def minSubsequence2(self, nums: List[int]) -> List[int]:
+        nums.sort(reverse=True)
+        i = 1
+        while sum(nums[:i]) <= sum(nums[i:]):
+            i += 1
+        return nums[:i]
+
+    def minSubsequence3(self, nums: List[int]) -> List[int]:
+        nums.sort(reverse=True)
+        left = nums[0]
+        sum_nums = sum(nums)
+        i = 1
+        while left <= sum_nums - left:
+            left += nums[i]
+            i += 1
+        return nums[:i]
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(Solution().minSubsequence([4, 3, 10, 9, 8]))
+    print(Solution().minSubsequence([4, 4, 7, 6, 7]))
+
+    print(Solution().minSubsequence([8, 8]))
+    print("--- %s seconds ---" % (time.time() - start_time))
